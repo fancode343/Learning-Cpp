94fa4983c291a2b7c22add13707eba516e843737
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-06-29 22:54:15 +0800

    Add Armstrong number calculation program
    Implement Armstrong number calculation in C++.

--- Problems/Armstrong.cpp
+++ Problems/Armstrong.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+#include <string> 
+#include <cmath>
+using namespace std;
+
+int mathPower(int number, int power);
+
+int main(){
+    cout << "Enter Number: ";
+    int number;
+    cin >> number;
+    string stringnum = to_string(number);
+    int ans = 0;
+    for(int i = 0; i<stringnum.length(); i++){
+        int Numlength = stringnum.length();
+        int num = stringnum[i];
+        int squared = mathPower(num, Numlength);
+        ans += squared;
+    }
+    cout << ans << " " << stringnum.length();
+    return 0;
+}
+
+int mathPower (int number, int power){
+    int result = 1;
+    for(int i = 0; i<power; i++){
+        result *= number;
+    }
+    return result;
+}
